2e440d674ffb831e49b7565164a2b3a4835e3a0e
Rodell <rodriguez.rodell53@gmail.com>
2024-06-19 17:40:36 -0400

    Override User toString method to print out params

--- backend/src/main/java/com/backend/users/User.java
+++ backend/src/main/java/com/backend/users/User.java
@@ -22,26 +22,31 @@ public class User {
     }
 
     public Long getId() {
-        return this.id;
-      }
+      return this.id;
+    }
     
-      public String getName() {
-        return this.name;
-      }
+    public String getName() {
+      return this.name;
+    }
     
-      public String getRole() {
-        return this.role;
-      }
+    public String getRole() {
+      return this.role;
+    }
     
-      public void setId(Long id) {
-        this.id = id;
-      }
+    public void setId(Long id) {
+      this.id = id;
+    }
     
-      public void setName(String name) {
-        this.name = name;
-      }
+    public void setName(String name) {
+      this.name = name;
+    }
     
-      public void setRole(String role) {
-        this.role = role;
-      }
+    public void setRole(String role) {
+      this.role = role;
+    }
+
+    @Override
+    public String toString() {
+      return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
+    }
 }
