b9be399f5730eef99ab58f3e34db36bf70768b39
Rodell <rodriguez.rodell53@gmail.com>
2024-06-21 19:08:34 -0400

    Unit tests for findById controller for existing and non existing User id

--- backend/src/test/java/com/backend/users/UserControllerTests.java
+++ backend/src/test/java/com/backend/users/UserControllerTests.java
@@ -1,13 +1,15 @@
 package com.backend.users;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -43,6 +45,36 @@ public class UserControllerTests {
         .andExpect(jsonPath("$.id").value(expectedUser.getId()))
         .andExpect(jsonPath("$.name").value(expectedUser.getName()))
         .andExpect(jsonPath("$.role").value(expectedUser.getRole()));
+    }
+
+    @Test
+    public void findById_ShouldReturnUser_WhenGivenExistingId() throws Exception {
+        Long existingId = 1L;
+        User expectedUser = new User(existingId, "Bob The Builder", "builder");
+        when(mockUserService.findById(existingId)).thenReturn(Optional.of(expectedUser));
+
+        mockMvc.perform(
+            get(String.format("/users/%s", existingId))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+        )
+        .andExpect(jsonPath("$.id").value(expectedUser.getId()))
+        .andExpect(jsonPath("$.name").value(expectedUser.getName()))
+        .andExpect(jsonPath("$.role").value(expectedUser.getRole()));
+    }
+
+    @Test
+    public void findById_ShouldReturnNotFound_WhenGivenNonExistingId() throws Exception {
+        Long nonExistingId = 1L;
+        when(mockUserService.findById(nonExistingId)).thenReturn(Optional.empty());
+
+        mockMvc.perform(
+            get(String.format("/users/%s", nonExistingId))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+        )
+        .andExpect(status().isNotFound())
+        .andDo(print());
     }
 
 }
